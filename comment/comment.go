e8de0e35951afc5efa638585c1a6c83bbdb6e375
Connor Zanin <cnnrznn@gmail.com>
2019-12-16 15:48:01 -0500

    comments have pointers to children for easy marshaling; add child and stringer methods

--- comment/comment.go
+++ comment/comment.go
@@ -5,17 +5,19 @@ import (
 )
 
 type Comment struct {
-	Text     string `json:"text"`
-	Parent   int    `json:"parent"`
-	Id       int    `json:"id"`
-	Children []int  `json:"children"`
-	Score    int    `json:"score"`
+	Text     string     `json:"text"`
+	Parent   int        `json:"parent"`
+	Id       int        `json:"id"`
+	Children []*Comment `json:"children"`
+	Score    int        `json:"score"`
 }
 
-func (c *Comment) AddChild(child int) {
+func (c *Comment) AddChild(child *Comment) {
+	// TODO lock mutex
 	c.Children = append(c.Children, child)
+	// TODO unlock mutex
 }
 
 func (c *Comment) String() string {
-	return fmt.Sprintf("%v:'%v'", c.Parent, c.Text)
+	return fmt.Sprintf("{%v: \n\t%v\n\t\n}", c.Text, c.Children)
 }
