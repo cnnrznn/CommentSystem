d43b85276b237c7c8588a3731ed1696b3900023c
Connor Zanin <cnnrznn@gmail.com>
2019-12-16 14:31:30 -0500

    methods for comment type

--- comment/comment.go
+++ comment/comment.go
@@ -1,9 +1,21 @@
 package comment
 
+import (
+	"fmt"
+)
+
 type Comment struct {
 	Text     string `json:"text"`
 	Parent   int    `json:"parent"`
 	Id       int    `json:"id"`
 	Children []int  `json:"children"`
 	Score    int    `json:"score"`
+}
+
+func (c *Comment) AddChild(child int) {
+	c.Children = append(c.Children, child)
+}
+
+func (c *Comment) String() string {
+	return fmt.Sprintf("%v:'%v'", c.Parent, c.Text)
 }
