832e3c2dc119b94082101006e4aa9c643e49616b
Connor Zanin <cnnrznn@gmail.com>
2019-12-17 13:58:29 -0500

    The site displays the comments as pretty-print JSON

--- site/js/script.js
+++ site/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
 $.get("http://localhost:8888/Comment/List?comment_id=0", function(data, status) {
-    console.log(status);
-    console.log(JSON.parse(data));
+    console.log(status, JSON.parse(data));
+
+    json = JSON.parse(data);
+
+    $('#comments').html(JSON.stringify(json, null, 2));
 });
 
 });
